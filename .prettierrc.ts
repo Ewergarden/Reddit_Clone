e1e4002bdf3a7ec630879ed98e091f0cce45a3c8
Vitaliy Pribylo <pribylo@ukr.net>
2023-10-05 22:07:41 +0300

    MB: Config files; install chakra ui.

---
+++ .prettierrc.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  printWidth: 120,
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { Providers } from "./providers";
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+    <body>
+    <Providers>
+      {children}
+    </Providers>
+    </body>
+    </html>
+  );
+}
+
+export default RootLayout;
